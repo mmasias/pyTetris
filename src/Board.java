79dbeff4cdb4a37e8ce6480c8a40b06263daeeb8
mmasias <manuel@masiasweb.com>
2025-06-24 13:09:28 +0200

    Agregar clases principales para el juego Tetris, incluyendo Board, Piece, Console, Position, Tetris y PieceFactory.

---
+++ src/Board.java
@@ -0,0 +1,167 @@
+public class Board {
+    private char[][] grid;
+    private int width;
+    private int height;
+    private Piece currentPiece;
+    private Console console;
+
+    public Board(int width, int height, Console console) {
+        this.width = width;
+        this.height = height;
+        this.grid = new char[height][width];
+        this.console = console;
+        clearBoard();
+    }
+
+    private void clearBoard() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                grid[i][j] = '.';
+            }
+        }
+    }
+
+    public void setCurrentPiece(Piece piece) {
+        this.currentPiece = piece;
+        piece.getPosition().setX(width / 2 - piece.getShape()[0].length / 2);
+        piece.getPosition().setY(0);
+    }
+
+    public Piece getCurrentPiece() {
+        return currentPiece;
+    }
+
+    public boolean canMovePiece(Piece piece, int deltaX, int deltaY) {
+        boolean[][] shape = piece.getShape();
+        Position pos = piece.getPosition();
+
+        for (int i = 0; i < shape.length; i++) {
+            for (int j = 0; j < shape[i].length; j++) {
+                if (shape[i][j]) {
+                    int newX = pos.getX() + j + deltaX;
+                    int newY = pos.getY() + i + deltaY;
+
+                    if (newX < 0 || newX >= width || newY >= height) {
+                        return false;
+                    }
+
+                    if (newY >= 0 && grid[newY][newX] != '.') {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean canRotatePiece(Piece piece, boolean clockwise) {
+
+        if (clockwise) {
+            piece.rotateClockwise();
+        } else {
+            piece.rotateCounterClockwise();
+        }
+
+        boolean canRotate = canMovePiece(piece, 0, 0);
+
+        if (!canRotate) {
+            if (clockwise) {
+                piece.rotateCounterClockwise();
+                piece.rotateCounterClockwise();
+                piece.rotateCounterClockwise();
+            } else {
+                piece.rotateClockwise();
+                piece.rotateClockwise();
+                piece.rotateClockwise();
+            }
+        }
+
+        return canRotate;
+    }
+
+    public void placePiece(Piece piece) {
+        boolean[][] shape = piece.getShape();
+        Position pos = piece.getPosition();
+
+        for (int i = 0; i < shape.length; i++) {
+            for (int j = 0; j < shape[i].length; j++) {
+                if (shape[i][j]) {
+                    int x = pos.getX() + j;
+                    int y = pos.getY() + i;
+                    if (y >= 0 && y < height && x >= 0 && x < width) {
+                        grid[y][x] = piece.getSymbol();
+                    }
+                }
+            }
+        }
+    }
+
+    public int clearCompleteLines() {
+        int linesCleared = 0;
+
+        for (int i = height - 1; i >= 0; i--) {
+            boolean isComplete = true;
+
+            for (int j = 0; j < width && isComplete; j++) {
+                if (grid[i][j] == '.') {
+                    isComplete = false;
+                }
+            }
+
+            if (isComplete) {
+                for (int k = i; k > 0; k--) {
+                    for (int j = 0; j < width; j++) {
+                        grid[k][j] = grid[k - 1][j];
+                    }
+                }
+
+                for (int j = 0; j < width; j++) {
+                    grid[0][j] = '.';
+                }
+
+                linesCleared++;
+                i++;
+            }
+        }
+
+        return linesCleared;
+    }
+
+    public void display() {
+        char[][] displayGrid = new char[height][width];
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                displayGrid[i][j] = grid[i][j];
+            }
+        }
+
+        if (currentPiece != null) {
+            boolean[][] shape = currentPiece.getShape();
+            Position pos = currentPiece.getPosition();
+
+            for (int i = 0; i < shape.length; i++) {
+                for (int j = 0; j < shape[i].length; j++) {
+                    if (shape[i][j]) {
+                        int x = pos.getX() + j;
+                        int y = pos.getY() + i;
+                        if (y >= 0 && y < height && x >= 0 && x < width) {
+                            displayGrid[y][x] = currentPiece.getSymbol();
+                        }
+                    }
+                }
+            }
+        }
+
+        console.clearScreen();
+        console.writeln("<!" + "=".repeat(((width + 2) * 2) - 1) + "!>");
+        for (int i = 0; i < height; i++) {
+            console.write("<!  ");
+            for (int j = 0; j < width; j++) {
+                console.write(displayGrid[i][j] + " ");
+            }
+            console.writeln(" !>");
+        }
+        console.writeln("<!" + "=".repeat(((width + 2) * 2) - 1) + "!>");
+        console.writeln("\\/".repeat(width + 4));
+    }
+}
---
+++ src/Tetris.java
@@ -0,0 +1,76 @@
+public class Tetris {
+    private Board board;
+    private boolean gameRunning;
+    private int score;
+    private Console console;
+
+    public Tetris(int width, int height) {
+        board = new Board(width, height, new Console());
+        gameRunning = true;
+        score = 0;
+        console = new Console();
+    }
+
+    public void spawnNewPiece() {
+        Piece[] pieces = {
+                PieceFactory.createIPiece(),
+                PieceFactory.createOPiece(),
+                PieceFactory.createTPiece(),
+                PieceFactory.createLPiece()
+        };
+
+        int randomIndex = (int) (Math.random() * pieces.length);
+        board.setCurrentPiece(pieces[randomIndex]);
+    }
+
+    private void processUserInput(String input) {
+        if (input.length() != 0) {
+            char command = input.charAt(0);
+
+            switch (command) {
+                case '4' -> {
+                    if (board.canMovePiece(board.getCurrentPiece(), -1, 0))
+                        board.getCurrentPiece().moveLeft();
+                }
+                case '6' -> {
+                    if (board.canMovePiece(board.getCurrentPiece(), 1, 0))
+                        board.getCurrentPiece().moveRight();
+                }
+                case '7' -> board.canRotatePiece(board.getCurrentPiece(), false);
+                case '9' -> board.canRotatePiece(board.getCurrentPiece(), true);
+            }
+        }
+    }
+
+    public void gameLoop() {
+
+        spawnNewPiece();
+        while (gameRunning) {
+            board.display();
+            String input = console.readString("Score: " + score + "\n\nComando (4=izq, 6=der, 7=rotar↺, 9=rotar↻): ");
+            processUserInput(input);
+
+            if (board.canMovePiece(board.getCurrentPiece(), 0, 1)) {
+                board.getCurrentPiece().moveDown();
+            } else {
+                board.placePiece(board.getCurrentPiece());
+
+                int linesCleared = board.clearCompleteLines();
+                score += linesCleared * 100;
+
+                if (linesCleared > 0) {
+                    console.writeln("¡" + linesCleared + " línea(s) eliminada(s)!");
+                    console.readString("Presiona Enter para continuar...");
+                }
+
+                spawnNewPiece();
+
+                if (!board.canMovePiece(board.getCurrentPiece(), 0, 0)) {
+                    gameRunning = false;
+                    console.writeln("¡GAME OVER!");
+                    console.writeln("Puntuación final: " + score);
+                }
+            }
+        }
+    }
+}
